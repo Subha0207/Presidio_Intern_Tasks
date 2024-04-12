231c70817c1b8b520c3a86d831b62ca576d9bea9
Subha0207 <subhashinisv@presidio.com>
2024-04-12 23:20:30 +0530

    Week1 tasks

---
+++ Day4/CARD_VALIDATION/CARD_VALIDATION/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Day4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            string input = Console.ReadLine();
+
+            // Input validation
+            if (input.Length > 15)
+            {
+                Console.WriteLine("Invalid input. Please enter a number not exceeding 15 digits.");
+                return;
+            }
+
+            char[] inputArray = input.ToCharArray();
+            int n = input.Length;
+            char[] reverse = new char[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                reverse[i] = inputArray[n - 1 - i];
+            }
+
+            int totalSum = 0;
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(reverse[i].ToString());
+                if ((i + 1) % 2 == 0)
+                {
+                    int evenPositionNumber = num * 2;
+                    int evenSum = 0;
+                    if (evenPositionNumber > 9)
+                    {
+                        while (evenPositionNumber != 0)
+                        {
+                            evenSum += evenPositionNumber % 10;
+                            evenPositionNumber /= 10;
+                        }
+                    }
+                    else
+                    {
+                        evenSum = evenPositionNumber;
+                    }
+                    totalSum += evenSum;
+                }
+                else
+                {
+                    totalSum += num;
+                }
+            }
+            Console.WriteLine(totalSum);
+            if (totalSum % 10 == 0)
+            {
+                Console.WriteLine("Valid number");
+            }
+            else
+            {
+                Console.WriteLine("Not a Valid number");
+            }
+        }
+    }
+}
---
+++ Day4/DOCTOR_OOPS/DOCTOR_OOPS/Program.cs
@@ -0,0 +1,122 @@
+﻿using DoctorClass.Models;
+using System;
+
+namespace DoctorClass.Models
+{
+    class Doctor
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Experience { get; set; }
+        public string Qualification { get; set; }
+        public string Specialization { get; set; }
+
+        public Doctor(int id) => Id = id;
+
+        public void PrintDetails()
+        {
+            Console.WriteLine($"Doctor Id\t:\t {Id}");
+            Console.WriteLine($"Doctor Name\t:\t {Name}");
+            Console.WriteLine($"Doctor Age\t:\t {Age}");
+            Console.WriteLine($"Doctor Experience\t:\t {Experience}");
+            Console.WriteLine($"Doctor Qualification\t:\t {Qualification}");
+            Console.WriteLine($"Doctor Specialization\t:\t {Specialization}");
+        }
+    }
+}
+
+namespace DoctorClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program pro = new Program();
+            Doctor[] docs = new Doctor[3];
+
+            for (int i = 0; i < docs.Length; i++)
+            {
+                docs[i] = pro.CreateDoctorDetails(101 + i);
+                Console.WriteLine("\n\n\n");
+            }
+
+            Console.WriteLine("\n\n\nDoctor Details:");
+            for (int i = 0; i < docs.Length; i++)
+            {
+                docs[i].PrintDetails();
+                Console.WriteLine("\n");
+            }
+
+            pro.FindDocBySpecialization(docs);
+        }
+
+        void FindDocBySpecialization(Doctor[] docs)
+        {
+            Console.WriteLine("\n\n\nFind Doctor by Specialization");
+            Console.WriteLine("Enter the Specialization to proceed further:");
+            string spcl = GetStringInput("Specialization");
+            bool flag = false;
+            for (int i = 0; i < docs.Length; i++)
+            {
+                if (docs[i].Specialization == spcl)
+                {
+                    flag = true;
+                    docs[i].PrintDetails();
+                }
+            }
+            if (!flag)
+            {
+                Console.WriteLine("Sorry we couldn't find any doctors!");
+            }
+        }
+
+        string GetStringInput(string FieldName)
+        {
+            string? inp;
+            do
+            {
+                inp = Console.ReadLine();
+
+                if (string.IsNullOrWhiteSpace(inp))
+                    Console.Write($"Invalid input, Please Enter {FieldName} again:\n");
+
+            } while (string.IsNullOrWhiteSpace(inp));
+            return inp;
+        }
+
+        int GetIntInput()
+        {
+            int num;
+            while (!int.TryParse(Console.ReadLine(), out num))
+            {
+                Console.WriteLine("Invalid entry. Please try again.");
+            }
+            return num;
+        }
+
+        Doctor CreateDoctorDetails(int id)
+        {
+            Console.WriteLine($"Doctor {id - 100} Detail\n");
+
+            Doctor doc = new Doctor(id);
+
+            Console.WriteLine("Enter Doctor Name:");
+            doc.Name = GetStringInput("Name");
+
+            Console.WriteLine("Enter Doctor Age:");
+            doc.Age = GetIntInput();
+
+            Console.WriteLine("Enter Doctor Experience:");
+            doc.Experience = GetIntInput();
+
+            Console.WriteLine("Enter Doctor Qualification:");
+            doc.Qualification = GetStringInput("Qualification");
+
+            Console.WriteLine("Enter Doctor Specialization:");
+            doc.Specialization = GetStringInput("Specialization");
+
+            return doc;
+        }
+    }
+}
