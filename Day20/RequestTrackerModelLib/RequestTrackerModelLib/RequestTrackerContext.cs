8072ba2855158540b38ea08dc73d9af2a66e095c
Subha0207 <subhashinisv@presidio.com>
2024-05-09 13:03:33 +0530

    Pair programming

---
+++ Day20/RequestTrackerModelLib/RequestTrackerModelLib/RequestTrackerContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RequestTrackerModelLib
+{
+    public class RequestTrackerContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=H0RBBX3\DEMOINSTANCE;Integrated Security=true;Initial Catalog=dbEmployeeTrackerCF;");
+        }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Request> Requests { get; set; }
+    }
+}
